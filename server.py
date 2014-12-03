342848275c83776baed859215862619e1ae16382
kmichala <kmichala@live.unc.edu>
2014-12-03 13:08:23 -0500

    Update server.py

--- server.py
+++ server.py
@@ -88,7 +88,7 @@ class Business(Resource):
         error_if_business_not_found(business_id)
         business=businesses[business_id]
         update = update_business_parser.parse_args()
-#        business['name'] = update['name']
+        business['name'] = update['name']
 #        business['location'] = update['location']
 #        business['URL'] = update['URL']
 #       business['phone'] = update['phone']
